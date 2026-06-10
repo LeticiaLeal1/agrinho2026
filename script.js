9f5769ff9e23cdb99f59651fdc50cb94caf11bd1
LeticiaLeal1 <leticia.leal.silva@escola.pr.gov.br>
2026-06-10 09:52:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function saibaMais() {
+    alert("A energia solar pode reduzir em até 95% os custos com eletricidade na propriedade rural.");
+}
 
+function calcular() {
+    let gasto = parseFloat(document.getElementById("gasto").value);
+
+    if (isNaN(gasto) || gasto <= 0) {
+        document.getElementById("resultado").innerHTML =
+            "Digite um valor válido.";
+        return;
+    }
+
+    let economiaAnual = gasto * 12 * 0.85;
+
+    document.getElementById("resultado").innerHTML =
+        `Economia estimada por ano: R$ ${economiaAnual.toFixed(2)}`;
+}
